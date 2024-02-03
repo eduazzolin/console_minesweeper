573a57eb3024156126b97eeb74abab1344d59690
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2024-02-03 12:00:08 -0300

    base working and colors

--- main.py
+++ main.py
@@ -1,52 +1,82 @@
 from model import *
 from model import Colors
 
+GAME_STATES = ['RUNNING', 'WIN', 'GAME_OVER']
 CAMP_SIZE: int = 9
-BOMB_QUANTITY: int = 10
-SEPARADOR = "  "
-COVERED_SYMBOL = "*"
-BOMB_SYMBOL = "💣"
+BOMB_QUANTITY: int = 20
+COVERED_SYMBOL = f'{Colors.B_GRAY} + {Colors.RESET}'
+BOMB_SYMBOL = f'{Colors.B_RED}{Colors.F_WHITE} * {Colors.RESET}'
 
-game_over = False
+game_state = 'RUNNING'
 camp = Camp(BOMB_QUANTITY, CAMP_SIZE)
 range_camp_size = range(0, CAMP_SIZE)
 view_y_axis = [str(i + 1) for i in range_camp_size]
 view_x_axis = [chr(i + 65) for i in range_camp_size]
 
 
-def show_camp():
-    global spot
+def show_camp(mode: str = 'NORMAL'):
     print(f'\n     {"  ".join(view_x_axis)}\n')
     for y in range_camp_size:
         print(f'{view_y_axis[y]}    ', end="")
         for x in range_camp_size:
             spot = [spot for spot in camp.spots if spot.y_axis == y if spot.x_axis == x][0]
             if spot.is_covered:
-                print(COVERED_SYMBOL, end=SEPARADOR)
+                if mode == 'NORMAL':
+                    print(COVERED_SYMBOL, end="")
+                if mode == 'ID':
+                    print(
+                        f'{Colors.F_RED if spot.is_bomb else ""}{spot.spot_id:02}{Colors.RESET if spot.is_bomb else ""}',
+                        end=" ")
             else:
-                print(spot.neighbours_bombs)
+                if spot.is_bomb:
+                    print(BOMB_SYMBOL, end="")
+                else:
+                    print(f'{Colors.NEIGHBOUR_BOMBS_COLORS[spot.neighbours_bombs]} {spot.neighbours_bombs} {Colors.RESET}', end="")
 
         print()
 
+
 def ask_coordinates():
-    coordinates = input('Enter the coordinates, for example, A1: ')
-    coordinate_x = view_x_axis.index(coordinates[:1])
+    coordinates = input('\nEnter the coordinates, for example, A1: ')
+    coordinate_x = view_x_axis.index(coordinates[:1].upper())
     coordinate_y = view_y_axis.index(coordinates[1:2])
-    spot = [spot for spot in camp.spots if spot.y_axis == coordinate_y if spot.x_axis == coordinate_x][0]
-    camp.discover(spot)
+    return [spot for spot in camp.spots if spot.y_axis == coordinate_y if spot.x_axis == coordinate_x][0]
 
 
-while not game_over:
+def discover(spot: Spot):
+    spot = camp.discover_spot(spot)
+    if spot.neighbours_bombs == 0:
+        camp.discover_neighbour_zeroes(spot)
+
+
+def check_game_state():
+    for spot in camp.spots:
+        if spot.is_bomb and not spot.is_covered:
+            return GAME_STATES[2]  # GAME_OVER
+
+    for spot in camp.spots:
+        if not spot.is_bomb and spot.is_covered:
+            return GAME_STATES[0]  # RUNNING
+
+    return GAME_STATES[1]  # WIN
+
+
+def end_game(game_state):
+    print('\n\n')
+    camp.reveal_camp()
     show_camp()
-    ask_coordinates()
+
+    if game_state == GAME_STATES[1]:
+        print('\n you won!')
+    if game_state == GAME_STATES[2]:
+        print('\n game over!')
 
 
+while game_state == GAME_STATES[0]:
+    show_camp(mode='ID')
+    show_camp(mode='NORMAL')
+    discover(ask_coordinates())
+    game_state = check_game_state()
 
-print('\n\n\n')
-last_y = 0
-for spot in camp.spots:
-    if spot.y_axis != last_y:
-        print("")
-        last_y = spot.y_axis
-    print(f'{Colors.RED if spot.is_bomb else ""}{spot.neighbours_bombs:02}{Colors.ENDC if spot.is_bomb else ""}',
-          end=" ")
+end_game(game_state)
+
--- model.py
+++ model.py
@@ -2,13 +2,80 @@ import random as rd
 
 
 class Colors:
-    RED = '\033[91m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    BLUE = '\033[94m'
-    GRAY = '\033[90m'
+    # Variables for ANSI escape codes for font colors
+    F_RED = '\033[38;2;255;0;0m'
+    F_GREEN = '\033[38;2;0;255;0m'
+    F_BLUE = '\033[38;2;0;0;255m'
+    F_YELLOW = '\033[38;2;255;255;0m'
+    F_PURPLE = '\033[38;2;128;0;128m'
+    F_ORANGE = '\033[38;2;255;165;0m'
+    F_CUSTOM = '\033[38;2;211;211;211m'  # Custom color (e.g., light gray)
+    F_CYAN = '\033[38;2;0;255;255m'
+    F_PINK = '\033[38;2;255;192;203m'
+    F_BROWN = '\033[38;2;139;69;19m'
+    F_GRAY = '\033[38;2;128;128;128m'
+    F_BLACK = '\033[38;2;0;0;0m'
+    F_WHITE = '\033[38;2;255;255;255m'
+    F_MAROON = '\033[38;2;128;0;0m'
+    F_OLIVE = '\033[38;2;128;128;0m'
+    F_TEAL = '\033[38;2;0;128;128m'
+    F_NAVY = '\033[38;2;0;0;128m'
+    F_SILVER = '\033[38;2;192;192;192m'
+    F_GOLD = '\033[38;2;255;215;0m'
+    F_INDIGO = '\033[38;2;75;0;130m'
+    F_ORCHID = '\033[38;2;218;112;214m'
+    F_DARKGREEN = '\033[38;2;0;100;0m'
+    F_TURQUOISE = '\033[38;2;64;224;208m'
+    F_FIREBRICK = '\033[38;2;178;34;34m'
+    F_SEASHELL = '\033[38;2;255;245;238m'
+
+    # Variables for ANSI escape codes for background colors
+    B_RED = '\033[48;2;255;0;0m'
+    B_GREEN = '\033[48;2;0;255;0m'
+    B_BLUE = '\033[48;2;0;0;255m'
+    B_YELLOW = '\033[48;2;255;255;0m'
+    B_PURPLE = '\033[48;2;128;0;128m'
+    B_ORANGE = '\033[48;2;255;165;0m'
+    B_CUSTOM = '\033[48;2;211;211;211m'  # Custom color (e.g., light gray)
+    B_CYAN = '\033[48;2;0;255;255m'
+    B_PINK = '\033[48;2;255;192;203m'
+    B_BROWN = '\033[48;2;139;69;19m'
+    B_GRAY = '\033[48;2;242;242;242m'
+    B_BLACK = '\033[48;2;0;0;0m'
+    B_WHITE = '\033[48;2;255;255;255m'
+    B_MAROON = '\033[48;2;128;0;0m'
+    B_OLIVE = '\033[48;2;128;128;0m'
+    B_TEAL = '\033[48;2;0;128;128m'
+    B_NAVY = '\033[48;2;0;0;128m'
+    B_SILVER = '\033[48;2;192;192;192m'
+    B_GOLD = '\033[48;2;255;215;0m'
+    B_INDIGO = '\033[48;2;75;0;130m'
+    B_ORCHID = '\033[48;2;218;112;214m'
+    B_DARKGREEN = '\033[48;2;0;100;0m'
+    B_TURQUOISE = '\033[48;2;64;224;208m'
+    B_FIREBRICK = '\033[48;2;178;34;34m'
+    B_SEASHELL = '\033[48;2;255;245;238m'
+
+    # Variables for ANSI escape codes for text effects
+    ITALIC = '\033[3m'
     BOLD = '\033[1m'
-    ENDC = '\033[0m'
+    UNDERLINE = '\033[4m'
+    BLINK = '\033[5m'
+    REVERSE = '\033[7m'
+    RESET = '\033[0m'
+
+    NEIGHBOUR_BOMBS_COLORS = {
+        0: B_WHITE,
+        1: '\033[48;2;230;255;238m',
+        2: '\033[48;2;255;255;230m',
+        3: '\033[48;2;255;242;230m',
+        4: '\033[48;2;255;240;250m',
+        5: '\033[48;2;255;240;250m',
+        6: '\033[48;2;230;255;230m',
+        7: '\033[48;2;255;230;230m',
+        8: '\033[48;2;255;179;179m',
+    }
+
 
 
 class Spot:
@@ -70,5 +137,21 @@ class Camp:
                 if neighbour.spot_id in spot.neighbours_ids and neighbour.neighbours_bombs == 0:
                     spot.neighbours_zeroes_ids.append(neighbour.spot_id)
 
-    def discover(self, spot: Spot):
-        pass
+    def discover_spot(self, spot: Spot):
+        spot.is_covered = False
+        return spot
+
+
+    def discover_neighbour_zeroes(self, spot: Spot):
+        sid = spot.spot_id
+        if len(spot.neighbours_zeroes_ids) > 0:
+            neighbour_spots_zeroes = [neighbour for neighbour in self.spots if
+                                      neighbour.spot_id in spot.neighbours_zeroes_ids if
+                                      neighbour.is_covered]
+            for neighbour in neighbour_spots_zeroes:
+                self.discover_spot(neighbour)
+                self.discover_neighbour_zeroes(neighbour)
+
+    def reveal_camp(self):
+        for spot in self.spots:
+            spot.is_covered = False
